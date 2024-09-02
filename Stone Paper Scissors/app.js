f2ad1b6f8e1912c278bb07a580e517b69e8aef34
abhinav-ponnakkari <abhinavponnakkari123@gmail.com>
2024-09-03 01:16:20 +0530

    stone3

--- Stone Paper Scissors/app.js
+++ Stone Paper Scissors/app.js
@@ -1,2 +1,51 @@
 let userScore=0;
 let compScore =0;
+
+const choices=document.querySelectorAll(".choice")
+
+const genCompChoice=()=>{
+    const options =["rock","paper","scissors"];
+    const randIdx=Math.floor(Math.random()*3);
+    return options[randIdx];
+}
+
+const drawGame=()=>{
+    console.log("game was draw.");
+}
+
+const showWinner=(userWin)=>{
+    if(userWin){
+        console.log("you win")
+    }else{
+      console.log("you lost")
+    }
+}
+
+const playGame=(userChoice)=>{
+    console.log("user choice ",userChoice)
+    //Generate computer choice
+    const compChoice=genCompChoice();
+    console.log("comp choice",compChoice);
+
+    if(userChoice === compChoice){
+        drawGame();
+    }else{
+        let userWin=true;
+        if(userChoice==="rock"){
+            userWin=compChoice ==="paper"?false:true;}
+        else if(userChoice==="paper"){
+            userWin=compChoice==="scissors"?false:true;
+        }else{
+            userWin=compChoice==="rock"?false:true;
+        }
+        showWinner(userWin);
+    }
+
+}
+
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
+    })
+})
